96338204dbd69d84a0d2066e18e2d878587ce370
Passuum2 <Darityreesemyles@gmail.com>
2025-04-28 20:55:48 -0500

    Added db connector and fixed variables

--- ChessSync.py
+++ ChessSync.py
@@ -3,9 +3,18 @@
 #
 from flask import Flask, render_template
 from modules import query_api
+from flask_mysqldb import MySQL
+from db import app
+from queries import *
 
-app = Flask(__name__)
-application = app
+mysql = MySQL(app)
+
+@app.route("/")
+def games():
+    cur = mysql.connection.cursor()
+    cur.execute(example_game_query)
+    rv = cur.fetchall()
+    return str(rv)
 
 @app.route('/')
 #Index route
@@ -19,13 +28,24 @@ def index():
 def search(name) -> str:
     #
     #
-    
     player = query_api(name)
     if not player:
         return f"Player '{name}' not found!"
 
     player = player.json()
     return f"Username: {player['username']}"
+
+@app.route('/s')
+#Index route
+def champtemp():
+    #index(): Renders index.html
+    #implementation: 
+    return render_template('chesschamp.html', )
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- modules.py
+++ modules.py
@@ -5,15 +5,16 @@
 #query_api(): Returns API database
 
 import requests      #get()
+import Worldchess    #champion.sql, eco.sql, elo.sql, games.sql, location.sql
+import re
 
-ip_adress = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+IP_ADDRESS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 API_URL = 'https://api.chess.com/pub/player/{}'
 
-def query_api(name) -> requests.Response:
-    #query_api(): Returns API database
+def query_api(sName) -> requests.Response:
+    #query_api(): Returns API database as request
     #Implementation: TODO
     #NOTE:CREDIT: https://support.chess.com/en/articles/9650547-published-data-api
-    response = requests.get(API_URL.format(name), headers=ip_adress)
-
-    return response
+    rReturn = requests.get(API_URL.format(sName), headers=IP_ADDRESS)
+    return rReturn
 
