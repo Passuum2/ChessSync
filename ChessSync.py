015f6c9740d2187f35c070e93739479c94dbbfa1
Passuum2 <Darityreesemyles@gmail.com>
2025-05-01 11:56:58 -0500

    WIP: local changes to ChessSync.py

--- ChessSync.py
+++ ChessSync.py
@@ -7,11 +7,13 @@ from flask import Flask, render_template     #render_template(), run()
 from flask_mysqldb import MySQL              #fetchall, fetchone(), execute(), connection.cursor(), MySQL()
 from modules import query_api                #query_api()
 from db import app                           #private
-from queries import *                        #private
+from queries import *                        #games, champion, elo, eco, location
 
 mysql = MySQL(app)
 
 def getDatabase(sqlQuery) -> tuple:
+    #getDatabase():
+    #Implementation:
     cur = mysql.connection.cursor()
     cur.execute(sqlQuery)
     sqlDB = cur.fetchall()
@@ -21,22 +23,23 @@ def getDatabase(sqlQuery) -> tuple:
 #Index route
 def index():
     #index(): Renders index.html
-    #implementation: 
+    #Implementation: 
     return render_template('index.html')
 
 # @app.route('/<name>')
 # #Second route# Get column names    tGames = cur.fetchall()
-
 #     player = player.json()
 #     return f"Username: {player['username']}"
 
-@app.route('/s')
+@app.route('/test')
 #Index route
 def champtemp():
-    #index(): Renders index.html
-    #implementation: 
-    dGames = dictgames()
-    return render_template('chesschamp.html', games = dGames)
+    #champtemp():
+    #Implementation: 
+    dGames = getDatabase(games)
+    dElo = getDatabase(elo)
+    dEco = getDatabase(eco)
+    return render_template('chesschamp.html', games = dGames, elo = dElo, eco = dEco)
 
 if __name__ == '__main__':
     app.run(debug=True)
